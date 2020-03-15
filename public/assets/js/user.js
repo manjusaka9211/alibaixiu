08b625d8ec3304c3f5fc9d4b6e02f9ea2aac2d41
彭妍 <1579986852@qq.com>
2020-03-15 12:01:56 +0800

    完成头像即时预览功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -2,8 +2,6 @@ let userArr = [];
 $.ajax({
     url: '/users',
     success: function (res) {
-        console.log(res);
-        
         userArr = res;
         rander();
     }
@@ -12,3 +10,22 @@ function rander() {
     var html = template('userList',{data: userArr});
     $('tbody').html(html);
 }
+// 头像上传表单发生改变时
+$('#modifyBox').on('change','#avatar',function() {
+    var formData = new FormData();
+    formData.append('avatar',this.files[0]);
+    $.ajax({
+        type: 'post',
+		url: '/upload',
+		data: formData,
+		// 告诉$.ajax方法不要解析请求参数
+		processData: false,
+		// 告诉$.ajax方法不要设置请求参数的类型
+		contentType: false,
+		success: function (response) {
+			// 实现头像预览功能
+			$('#preview').attr('src', response[0].avatar);
+			$('#hiddenAvatar').val(response[0].avatar)
+		}
+    })
+})
