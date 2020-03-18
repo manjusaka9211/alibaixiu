ed096bc4f7f1460e2b599ac4c6c110429b494214
彭妍 <1579986852@qq.com>
2020-03-18 16:55:40 +0800

    完成文章删除功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -23,7 +23,9 @@ function rander(cid, state, page = 1) {
 // 页面加载发送请求获取数据
 rander(cid,state);
 // 分页功能
+var num = 1;
 function changePage(index) {
+    num = index;
     rander(cid,state,index);
 }
 // 获取分类列表显示到页面上
@@ -40,4 +42,27 @@ $('#search').on('click', function () {
     state = $('#state').val();
     // 获取数据渲染页面
     rander(cid,state);
+});
+// 文章删除功能
+$('tbody').on('click','.del',function () {
+    // 获取当前点击的文章的id
+    let id = $(this).attr('data-id');
+    if (confirm('您确定要删除这篇文章吗？')) {
+        $.ajax({
+            type: 'delete',
+            url: '/posts/' + id,
+            success: function (res) {
+                // 判断当前tbody下的tr数，如果只有一个，跳转到上一页
+                if (num != 1) {
+                    if ($('tbody tr').length == 1) {
+                        rander(cid, state, --num);
+                    } else {
+                        rander(cid, state, num);
+                    }
+                } else {
+                    rander(cid, state, num);
+                }
+            }
+        });
+    }
 });
